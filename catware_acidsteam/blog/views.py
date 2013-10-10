485bbc672fba211dcb81839a3bde914379c15280
P4R4D0X1 Blu3Haz <P4R4D0X1@catware.fr>
2013-10-10 18:46:44 +0200

    master update

--- catware_acidsteam/blog/views.py
+++ catware_acidsteam/blog/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+#-*- coding: utf-8 -*-
+from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+
+def home(request):
+	text = """<center><h1>CatWare-4.0.0.1-AcidSteaM #dev</h1></br>
+		<p>P4R4D0X1 Prod</p></center>"""
+	return HttpResponse(text)
+
+def list_articles(request, month, year):
+	text = "Vous avez demandé les articles de {0} {1}".format(month, year)
+	return HttpResponse(text)		
+
+def tpl(request):
+	return render(request, 'blog/tpl.html', {'current_date': datetime.now()})
+	
+def addition(request, number1, number2):
+	total = int(number1) + int(number2)
+	return render(request, 'blog/addition.html', locals())		
--- catware_acidsteam/catware_acidsteam/urls.py
+++ catware_acidsteam/catware_acidsteam/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^blog/', include('blog.urls')),
     # Examples:
     # url(r'^$', 'catware_acidsteam.views.home', name='home'),
     # url(r'^catware_acidsteam/', include('catware_acidsteam.foo.urls')),
@@ -15,3 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
