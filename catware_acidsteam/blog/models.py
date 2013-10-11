36d75184600a0d19bf254fd48030a5ab0b97b49d
P4R4D0X1 Blu3Haz <P4R4D0X1@catware.fr>
2013-10-11 13:02:52 +0200

    adding categories

--- catware_acidsteam/blog/models.py
+++ catware_acidsteam/blog/models.py
@@ -6,7 +6,14 @@ class Article(models.Model):
 	auteur = models.CharField(max_length=42)
 	contenu = models.TextField(null=True)
 	date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de publication")
-	
+	categorie = models.ForeignKey('Categorie')
+
 	def __unicode__(self):
 	
-		return u"%s" % self.titre 
+		return self.titre 
+
+class Categorie(models.Model):
+	nom = models.CharField(max_length=30)
+
+	def __unicode__(self):
+		return self.nom
