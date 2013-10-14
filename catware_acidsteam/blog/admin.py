4db308238f55e678cc257845d207a822b666b14b
P4R4D0X1 Blu3Haz <P4R4D0X1@catware.fr>
2013-10-14 23:49:20 +0200

    update of admin interface

--- catware_acidsteam/blog/admin.py
+++ catware_acidsteam/blog/admin.py
@@ -1,12 +1,24 @@
+#-*- coding:utf-8 -*-
 from django.contrib import admin
 from blog.models import Article, Categorie
 
 class ArticleAdmin(admin.ModelAdmin):
-	list_display	= ('titre', 'auteur', 'date')
+	list_display	= ('titre', 'auteur', 'date', 'apercu_contenu')
 	list_filter	= ('auteur', 'categorie',)
 	date_hierarchy	= 'date'
 	ordering	= ('date', )
 	search_fields	= ('titre', 'contenu')
+	fields		= ('titre', 'slug', 'auteur', 'categorie', 'contenu')
 
+	def apercu_contenu(self, article):
+		"""retourne les 40 premiers caractères d'un article s'il y en a plus complète avec des points de suspension """
+		text = article.contenu[0:40]
+		if len(article.contenu) > 40:
+			return '%s...' % text
+		else:
+			return text
+		
+	apercu_contenu.short_description = u"apercu du contenu"
+			
 admin.site.register(Categorie)
 admin.site.register(Article, ArticleAdmin)
