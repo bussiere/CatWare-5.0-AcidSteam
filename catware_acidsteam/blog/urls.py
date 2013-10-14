750ab0c2981ed73a39c62e5af2a540a234fb86be
P4R4D0X1 Blu3Haz <P4R4D0X1@catware.fr>
2013-10-14 12:04:26 +0200

    adding slug and article

--- catware_acidsteam/blog/urls.py
+++ catware_acidsteam/blog/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('blog.views',
-	url(r'^acceuil/$', 'home'),
-	url(r'^$', 'tpl'),
+	url(r'^$', 'home'),
+	url(r'^article/(?P<id>\d+)-(?P<slug>.+)$', 'read'),
 	url(r'^addition/(?P<number1>\d+)/(?P<number2>\d+)/$', 'addition'),
-	url(r'^article/(?P<id_article>\d+)/$', 'view_article'),
 	url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/$', 'list_articles'),
 )	
