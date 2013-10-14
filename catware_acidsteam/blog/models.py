85dbf26b5b137f9a6f5d5b2109f23490eaa4f6c9
P4R4D0X1 Blu3Haz <P4R4D0X1@catware.fr>
2013-10-14 12:01:02 +0200

    article function update

--- catware_acidsteam/blog/models.py
+++ catware_acidsteam/blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Article(models.Model):
 	titre = models.CharField(max_length=100)
+	slug  = models.SlugField(max_length=100)
 	auteur = models.CharField(max_length=42)
 	contenu = models.TextField(null=True)
 	date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Date de publication")
