eb9da8fb5033c90a7bb08e0bf3e067316085878d
P4R4D0X1 Blu3Haz <P4R4D0X1@catware.fr>
2013-10-15 21:32:12 +0200

    adding the fieldsets

--- catware_acidsteam/blog/admin.py
+++ catware_acidsteam/blog/admin.py
@@ -3,12 +3,26 @@ from django.contrib import admin
 from blog.models import Article, Categorie
 
 class ArticleAdmin(admin.ModelAdmin):
+	
+	#configuration de la liste des articles
 	list_display	= ('titre', 'auteur', 'date', 'apercu_contenu')
 	list_filter	= ('auteur', 'categorie',)
 	date_hierarchy	= 'date'
 	ordering	= ('date', )
 	search_fields	= ('titre', 'contenu')
-	fields		= ('titre', 'slug', 'auteur', 'categorie', 'contenu')
+	prepopulated_fields = {'slug': ('titre', ), }
+
+	#configuration du formulaire d'edition	
+	fieldsets = (
+		('Générale',  {
+			'classes': ['collapse',],
+			'fields': ('titre', 'slug', 'auteur', 'categorie')
+		}),	
+		('contenu de l\'article', {
+			'description': u'Le formulaire accepte les balises html',
+			'fields': ('contenu', )
+		}),	
+	)
 
 	def apercu_contenu(self, article):
 		"""retourne les 40 premiers caractères d'un article s'il y en a plus complète avec des points de suspension """
